9acfbff8e332b86b819db65a7ef76220c34b8652
Brian Takita & Bruce Krysiak <pair-brian.takita-and-bruce.krysiak@pivotallabs.com>
2009-05-11 10:01:17 -0700

    Added Initial selenium_rc code. Going to figure out a way to automate adding the SeleniumRC jar file without committing it to the repo.

---
+++ lib/selenium_rc.rb
@@ -0,0 +1,3 @@
+dir = File.dirname(__FILE__)
+require "selenium"
+require "#{dir}/selenium_rc/server"
---
+++ lib/selenium_rc/server.rb
@@ -0,0 +1,70 @@
+module SeleniumRC
+
+  class Server
+
+    def self.boot
+      new.boot
+    end
+
+    def boot
+      start
+      wait
+      stop_at_exit
+    end
+
+    def start
+      remote_control.start
+    end
+
+    def stop_at_exit
+      at_exit do
+        stop
+      end
+    end
+
+    def remote_control
+      return @remote_control if @remote_control
+
+      @remote_control = ::Selenium::RemoteControl::RemoteControl.new("0.0.0.0", Webrat.configuration.selenium_server_port, 5)
+      @remote_control.jar_file = jar_path
+
+      return @remote_control
+    end
+
+    def jar_path
+      File.expand_path(__FILE__ + "../../../../../vendor/selenium-server.jar")
+    end
+
+    def wait
+      $stderr.print "==> Waiting for Selenium RC server on port #{Webrat.configuration.selenium_server_port}... "
+      wait_for_socket
+      $stderr.print "Ready!\n"
+    rescue SocketError
+      fail
+    end
+
+    def wait_for_socket
+      silence_stream(STDOUT) do
+        TCPSocket.wait_for_service_with_timeout \
+            :host     => (Webrat.configuration.selenium_server_address || "0.0.0.0"),
+          :port     => Webrat.configuration.selenium_server_port,
+          :timeout  => 15 # seconds
+      end
+    end
+
+    def fail
+      $stderr.puts
+      $stderr.puts
+      $stderr.puts "==> Failed to boot the Selenium RC server... exiting!"
+      exit
+    end
+
+    def stop
+      silence_stream(STDOUT) do
+        ::Selenium::RemoteControl::RemoteControl.new("0.0.0.0", Webrat.configuration.selenium_server_port, 5).stop
+      end
+    end
+
+  end
+
+end
